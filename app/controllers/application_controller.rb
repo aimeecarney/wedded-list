fbcc11de9a78e3d23cac455da30ef2203d2b2829
Aimee Carney <acarney29@gmail.com>
2017-10-24 03:14:11 +0000

    have authenticated routing working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :logged_in?, :current_user
-  before_action :current_user
-  before_action :authenticate_user, except: [:new, :create, :home]
-
-  private
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    !!current_user
-  end
-
-  def authenticate_user
-    redirect_to root_path unless logged_in?
-  end
+  # helper_method :logged_in?, :current_user
+  # before_action :current_user
+  # before_action :authenticate_user, except: [:new, :create, :home]
+  #
+  # private
+  #
+  # def current_user
+  #   @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  # end
+  #
+  # def logged_in?
+  #   !!current_user
+  # end
+  #
+  # def authenticate_user
+  #   redirect_to root_path unless logged_in?
+  # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   resources :lists
   resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    root 'welcome#home'
+    # root 'welcome#home'
+    authenticated do
+      root :to => 'lists#index'
+    end
+    root :to => 'welcome#home'
+
     devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 end
