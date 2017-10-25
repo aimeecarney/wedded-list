ab54e3351d6cfdfcda3b4ca75908a258bed9e88c
Aimee Carney <acarney29@gmail.com>
2017-10-25 17:38:30 +0000

    added contacts to form

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -11,7 +11,7 @@ class ListsController < ApplicationController
     @list = List.new(list_params)
     @list.user_id = current_user.id if current_user
     @list.save
-    redirect_to '/lists/show'
+    redirect_to '/lists/:id'
   end
 
   def show
