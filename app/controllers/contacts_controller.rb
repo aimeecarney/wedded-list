389f6ab571a325ffd04305613450748dd47ff372
Aimee Carney <acarney29@gmail.com>
2017-10-25 03:24:15 +0000

    added form and views for contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,43 @@
 class ContactsController < ApplicationController
+
+    def new
+      @contact = Contact.new
+    end
+
+    def index
+      @contacts = Contact.all
+    end
+
+    def create
+      @contact = Contact.new(contact_params)
+      @contact.save
+      redirect_to '/contacts/index'
+    end
+
+    def show
+      @contact = Contact.find(params[:id])
+    end
+
+    def update
+      @contact = Contact.find(params[:id])
+      @contact.update(contact_params)
+      redirect_to contacts_path(@contact)
+    end
+
+    def edit
+      @contact = Contact.find(params[:id])
+    end
+
+    def destroy
+      @contact = Contact.find(params[:id])
+      @contact.destroy
+      redirect_to contacts_path(@contact)
+    end
+
+    private
+
+    def contact_params
+      params.require(:contact).permit(:name, :address, :phone_number, :email, :plus_one_name)
+    end
+
 end
