ece90fb2fe64ac6d7b153eb8c03f9c0300adcfae
Aimee Carney <acarney29@gmail.com>
2018-01-21 21:57:34 +0000

    made events a partial to load

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -13,7 +13,7 @@ function loadEvents() {
   $.ajax({
     method: "GET",
     url: this.href
-  }).done(function(data){
-    console.log(data)
+  }).done(function(response){
+    $("div.events").html(response)
   })
 }
