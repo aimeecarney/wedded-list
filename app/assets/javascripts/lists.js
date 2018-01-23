ed9e4459562a7ef58b555d6da2c61e783c0ff383
Aimee Carney <acarney29@gmail.com>
2018-01-23 02:00:51 +0000

    have url loading with ajax for local , just need to render data

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -1,6 +1,11 @@
 $(function(){
   $('.load_contacts').on('click', function(e){
-    alert('hello')
+    $.ajax({
+      method: "GET",
+      url: this.href
+    }).done(function(data){
+      $("div.local_contacts_list").html(data)
+    })
     e.preventDefault()
   })
 })
