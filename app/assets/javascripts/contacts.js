40c84f46e10a2d75b809842da997b6cc75854425
Aimee Carney <acarney29@gmail.com>
2018-01-27 16:31:46 +0000

    adjusted view for local contacts to better fit ajax output

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -1,6 +1,11 @@
 $(function(){
-  $('.events').on('click', function(e){
-    alert("hello")
+  $('.load_events').on('click', function(e){
+    $.ajax({
+      method: "GET",
+      url: this.href
+    }).done(function(data){
+      $("div.events_list").html(data)
+    })
     e.preventDefault()
   })
 })
--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -5,7 +5,6 @@ $(function(){
       url: this.href
     }).done(function(data){
       $("div.local_contacts_list").html(data)
-
     })
     e.preventDefault()
   })
