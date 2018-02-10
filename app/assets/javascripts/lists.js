9b4bcfb46532c0875590af159e097e7f672fdf9e
Aimee Carney <acarney29@gmail.com>
2018-02-10 18:25:01 +0000

    moved js to lists.js file instead of show page

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -9,4 +9,21 @@ $(function(){
 
     e.preventDefault()
   })
+
+    $(".js-next").on("click", function() {
+      var nextId = parseInt($(".js-next").attr("data-id")) + 1;
+      $.getJSON("/lists/" + nextId + "/list_data", function(data) {
+        $(".listName").text(data["name"]);
+        $(".listDate").text(data["date"]);
+        $(".listCity").text(data["city"]);
+        $(".listState").text(data["state"]);
+        $(".guestlist").text("");
+        $.each(data.contacts, function(index){
+            $(".guestlist").append(this.name + "<BR>")
+          })
+        // re-set the id to current on the link
+        $(".js-next").attr("data-id", data["id"]);
+      });
+    });
+
 })
